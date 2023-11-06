92f7427113dae471e953fbdad25236026b119c85
caionikolas <Caio.kanu@hotmail.com>
2023-11-05 23:26:15 -0300

    tratamentos de erros

--- src/app/repositorios/vendas.repositorio.ts
+++ src/app/repositorios/vendas.repositorio.ts
@@ -1,5 +1,7 @@
+import { VendaId } from '@app/use-cases/cancelar-venda';
 import { Venda } from '../entidades/venda/venda';
 
 export abstract class VendasRepositorios {
   abstract create(venda: Venda): Promise<void>;
+  abstract findById(vendaId: VendaId): Promise<Venda | null>;
 }
---
+++ src/app/use-cases/cancelar-venda.ts
@@ -0,0 +1,35 @@
+import { VendasRepositorios } from '../repositorios/vendas.repositorio';
+import { Injectable } from '@nestjs/common';
+import { VendaNaoEncontrada } from './erros/venda-not-found';
+
+interface CancelarVendaRequest {
+  produtoId: string;
+  compradorId: string;
+}
+
+export interface VendaId {
+  produtoId: string;
+  compradorId: string;
+}
+
+type CancelarVendaResponse = void;
+
+@Injectable()
+export class CancelarVenda {
+  constructor(private vendasRepositorios: VendasRepositorios) {}
+
+  async execute(request: CancelarVendaRequest): Promise<CancelarVendaResponse> {
+    const { produtoId, compradorId } = request;
+
+    const vendaId = {
+      produtoId,
+      compradorId,
+    };
+
+    const venda = await this.vendasRepositorios.findById(vendaId);
+
+    if (!venda) {
+      throw new VendaNaoEncontrada();
+    }
+  }
+}
